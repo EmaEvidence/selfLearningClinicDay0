825ebe99913b5105f7b01bb5e892ed48b980159a
EmaEvidence <emmanuelalabi563@gmail.com>
2017-04-08 02:06:36 +0100

    Added Fizzbuzz Solution

---
+++ jasmine-standalone-2.5.2/src/fizzBuzz.js
@@ -0,0 +1,15 @@
+function fizzBuzz(num){
+	if ((num % 5 == 0) && (num % 3 == 0)){
+		return 'FizzBuzz';
+	}
+	else if (num % 5 == 0){
+		return 'Buzz';
+	}
+	else if (num % 3 == 0) {
+		return 'Fizz';
+	}
+	else{
+		return num;
+	}
+
+}
